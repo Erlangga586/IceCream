a6acc56dbd779d4cfdb8c85f5966caedd8e5831d
Erlangga586 <73045741+Erlangga586@users.noreply.github.com>
2022-01-02 18:08:28 +0700

    b860hv5-os9/loading.sh

---
+++ loading.sh
@@ -0,0 +1,14 @@
+echo -n '>>>                       [20%]\r'
+# some task
+sleep 2
+echo -n '>>>>>>>                   [40%]\r'
+# some task
+sleep 2
+echo -n '>>>>>>>>>>>>>>            [60%]\r'
+# some task
+sleep 2
+echo -n '>>>>>>>>>>>>>>>>>>>>>>>   [80%]\r'
+# some task
+sleep 2
+echo -n '>>>>>>>>>>>>>>>>>>>>>>>>>>[100%]\r'
+echo -n '\n'
